727f5aefb0a018da505a90286a89b609f6c1a858
unknown <montinformatic233@gmail.com>
2023-03-30 20:58:04 -0500

    Done for  documentation

--- database/migrations/2023_02_25_191323_categories.php
+++ database/migrations/2023_02_25_191323_categories.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('id_name_category');
             $table->string('description_category');
             $table->timestamps();
         });
